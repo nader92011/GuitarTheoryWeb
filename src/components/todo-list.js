8f25fda2582cf610524d24e422a073a03c2a2768
Andrew Peirs <peirs.andrew@gmail.com>
2016-10-31 19:35:43 +0000

    Basic list of dynamic questions

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,23 +2,65 @@ import React from 'react';
 
 export default class TodoList extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      questions: [
+        'What is the name of the ' + generateRandomStringNumber() + ' string?',
+        'Which number string is the ' + generateRandomStringName() + ' string?',
+        'Say out loud all of the roots in each pattern position',
+        'Say what notes have half steps',
+        'Say name of the note on the ' + generateRandomStringName() + ' string at the ' + generateRandomFretNumber() + ' fret',
+        'Say Major Scale',
+        'Play Major scale on the ' + generateRandomStringName() + ' string starting at the ' + generateRandomFretNumber() + ' fret - (Say note names)',
+        'Play all 5 major scale patterns',
+        'Say Minor Scale',
+        'Play Minor scale on the ' + generateRandomStringName() + ' string starting at the ' + generateRandomFretNumber() + ' fret - (Say note names)',
+        'Play all 5 Minor scale patterns',
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
         <ul>
-          <li>What is the name of the nth string?</li>
-          <li>Which number string to the "" string?</li>
-          <li>Say out loud all of the roots in each pattern position</li>
-          <li>Say what notes have half steps</li>
-          <li>Say name of note at n string on n fret</li>
-          <li>Say Major Scale</li>
-          <li>Play Major scale on n string starting at n fret<br />(Say note names)</li>
-          <li>Play all 5 major scale patterns</li>
-          <li>Say Minor Scale</li>
-          <li>Play Minor scale on n string starting at n fret<br />(Say note names)</li>
-          <li>Play all 5 minor scale patterns</li>
+          {
+            this.state.questions.map(question => {
+              return <li>{question}</li>
+            })
+          }
         </ul>
       </div>
     )
   }
+}
+
+const STRING_NUMBERS = [
+  '1st', '2nd', '3rd', '4th', '5th', '6th',
+]
+
+function generateRandomStringNumber() {
+  var index = Math.floor(Math.random() * 6);
+  return STRING_NUMBERS[index];
+}
+
+const STRING_NAMES = [
+  'E', 'B', 'G', 'D', 'A', 'E',
+]
+
+function generateRandomStringName() {
+  var index = Math.floor(Math.random() * 6);
+  return STRING_NAMES[index];
+}
+
+const FRET_NUMBERS = [
+  '1st', '2nd', '3rd', '4th', '5th', '6th',
+  '7th', '8th', '9th', '10th', '11th', '12th',
+]
+
+function generateRandomFretNumber() {
+  var index = Math.floor(Math.random() * 12) + 1;
+  return FRET_NUMBERS[index];
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
 import Layout from './components/layout'
 
 render(<Layout />, document.getElementById('root'));
